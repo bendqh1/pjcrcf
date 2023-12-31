3da7b6088d6ef5fbc25b10b10c4ec8ca70063008
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-12-31 18:54:35 +0200

    Update index_body_success.php

--- index_body_success.php
+++ index_body_success.php
@@ -1,6 +1,6 @@
 <div dir="rtl" id="prcf_success_wrapper">
     <div class="prcf_container">
-        <h2 class="prcf_header_headings">הודעתך נשלחה בהצלחה</h2><br>
+        <h2 class="prcf_header_headings">הודעתך נשלחה בהצלחה</h2>
         <p class="prcf_header_p">תודה על ההתעניינות לעבוד עימי ועל כל פנייה המצריכה את תגובתי אשתדל להגיב בהקדם האפשרי.</p>
     </div>
 </div>
